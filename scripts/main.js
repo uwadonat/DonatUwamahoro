985b08dea4786a079b6fd1ac74754419082c7499
uwadonat <uwadonat@gmail.com>
2021-05-03 11:48:42 +0200

    add changing images

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/image7.jpg') {
+      myImage.setAttribute('src','images/donat2.jpg');
+    } else {
+      myImage.setAttribute('src','images/image7.jpg');
+    }
+}
+
+
